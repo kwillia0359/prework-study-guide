dffb74dfd3b9451a1a137d68d33251b0b4c58634
Kaylah Williams <kaylahwilliams@Kaylahs-MacBook-Pro.local>
2023-06-19 12:28:31 -0600

    added JavaScript functions to Prework Study Guide

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,24 @@
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for (var x = 0; x < topics.length; x++) {
- console.log(topics[x]);
+function listTopics() {
+ for (var x = 0; x < topics.length; x++) {
+   console.log(topics[x]);
+ }
 }
 
-/*
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
+function selectTopic() {
+ if (randomTopic[0] === 'HTML') {
+   console.log("Let's study HTML!");
+ } else if (randomTopic[0] === 'CSS') {
+   console.log("Let's study CSS!");
+ } else if (randomTopic[0] === 'Git') {
+   console.log("Let's study Git!");
+ } else if (randomTopic[0] === 'JavaScript') {
+   console.log("Let's study JavaScript!");
+ } else {
+   console.log('Please try again!');
+ }
 }
-*/
+listTopics()
+selectTopic()
